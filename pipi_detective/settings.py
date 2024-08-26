677d5d6e6079901f0c24055cafd6a80149bdb77d
Peter Matkovski <pmatkovski@atlassian.com>
2024-08-26 11:34:21 +0200

    vagrant golden image implemented

--- pipi_detective/settings.py
+++ pipi_detective/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # for sqlite write lock timeout
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
